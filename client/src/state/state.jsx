220cedb40256b6c98484bd346d11e09171cc7eb3
AgentSandbox <pregrada@mail.com>
2024-05-16 22:28:21 +0200

    com9

--- client/src/state/state.jsx
+++ client/src/state/state.jsx
@@ -8,38 +8,54 @@ export const store = [
             {
                 id: v1(),
                 check: false,
-                nameProject: "Auser000",
+                nameProject: "Var1",
                 def: [
-                    {file: "kinkiTypeOneDefoult1"},
-                    {file: "kinkiTypeOneDefoult2"},
-                    {file: "kinkiTypeOneDefoult3"},
-                    {file: "kinkiTypeOneDefoult4"},
-                    {file: "kinkiTypeOneDefoult5"},
-                    {file: "kinkiTypeOneDefoult6"},
-                    {file: "kinkiTypeOneDefoult7"},
-                    {file: "kinkiTypeOneDefoult8"},
-                    {file: "kinkiTypeOneDefoult9"},
+                    {file: "Var1A"},
+                    {file: "Var1B"},
                 ],
                 components: [
-                    {id: v1(), groupElements: 0, name: "ViewOne", file: "kinkiOneViewOne", check: true},
-                    {id: v1(), groupElements: 0, name: "ViewTwo", file: "kinkiOneViewTwo", check: false},
-                    {id: v1(), groupElements: 1, name: "balka", file: "balka", check: false},
-                    {id: v1(), groupElements: 1, name: "balkaFalse", file: "balkaFalse", check: true},
-                    {id: v1(), groupElements: 2, name: "bad", file: "bad", check: false},
-                    {id: v1(), groupElements: 2, name: "badFalse", file: "badFalse", check: true},
+                    // {id: v1(), groupElements: 0, name: "ViewOne", file: "kinkiOneViewOne", check: true},
+                    // {id: v1(), groupElements: 0, name: "ViewTwo", file: "kinkiOneViewTwo", check: false},
                 ]
             },
             {
                 id: v1(),
                 check: false,
-                nameProject: "Auser001",
+                nameProject: "Var2",
                 def: [
-                    {file: "kinkiTypeOneDefoult1"},
-                    {file: "kinkiTypeOneDefoult2"},
+                    {file: "Var2A"},
+                    {file: "Var2B"},
                 ],
                 components: [
-                    {id: v1(), groupElements: 0, name: "ViewOne", file: "kinkiOneViewOne", check: true},
-                    {id: v1(), groupElements: 0, name: "ViewTwo", file: "kinkiOneViewTwo", check: false},
+                    // {id: v1(), groupElements: 0, name: "ViewOne", file: "kinkiOneViewOne", check: true},
+                    // {id: v1(), groupElements: 0, name: "ViewTwo", file: "kinkiOneViewTwo", check: false},
+                ]
+            },
+            {
+                id: v1(),
+                check: false,
+                nameProject: "Var3",
+                def: [
+                    {file: "Var3A"},
+                    {file: "Var3B"},
+                    {file: "Var3C"},
+                ],
+                components: [
+                    // {id: v1(), groupElements: 0, name: "ViewOne", file: "kinkiOneViewOne", check: true},
+                    // {id: v1(), groupElements: 0, name: "ViewTwo", file: "kinkiOneViewTwo", check: false},
+                ]
+            },
+            {
+                id: v1(),
+                check: false,
+                nameProject: "Var4",
+                def: [
+                    {file: "Var4A"},
+                    {file: "Var4B"},
+                ],
+                components: [
+                    // {id: v1(), groupElements: 0, name: "ViewOne", file: "kinkiOneViewOne", check: true},
+                    // {id: v1(), groupElements: 0, name: "ViewTwo", file: "kinkiOneViewTwo", check: false},
                 ]
             }
         ]
